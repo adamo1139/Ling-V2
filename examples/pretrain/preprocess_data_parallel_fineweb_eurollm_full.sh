8ec6fab930f83ef5324c1691aafef6e097c0861e
adamo1139 <adamo1139@gmail.com>
2025-09-15 11:34:41 +0200

    update

--- examples/pretrain/preprocess_data_parallel_fineweb_eurollm_full.sh
+++ examples/pretrain/preprocess_data_parallel_fineweb_eurollm_full.sh
@@ -4,7 +4,7 @@ set -ex
 # Parallel preprocessing script for large parquet files (280GB+)
 # Splits the file into chunks and processes them in parallel for maximum speed
 
-TOKENIZER_PATH=`dirname $(readlink -f "${BASH_SOURCE[0]}")`/../../resource/tokenizer/euroLLM/tokenizer.model
+TOKENIZER_PATH="/home/ubuntu/preprocessing/Ling-V2/resource/tokenizer/euroLLM/tokenizer.model"
 MEGATRON_PATH="Megatron-LM-core_v0.13.0"
 
 PYTHONPATH=${MEGATRON_PATH}:$PYTHONPATH python3 /home/ubuntu/preprocessing/Ling-V2/preprocess_large_parquet_parallel.py \
