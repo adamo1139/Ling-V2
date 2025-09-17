bcb5ea59f615022a76a7cd4793f1275dcfd1cf63
adamo1139 <adamo1139@gmail.com>
2025-09-18 00:51:14 +0200

    update

--- examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
+++ examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
@@ -130,7 +130,7 @@ TRAINING_ARGS=(
     --fp8-recipe "blockwise"
     --fp8-format "e4m3"
 
-    --optimizer "adam"
+    --optimizer "adam_bnb_8bit"
     --lr "7.0e-4"
     --lr-decay-style cosine
     --min-lr "3.0e-5"
