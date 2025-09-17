e4f68503f79eab488ff6e82e82f6fbe2746ba01e
adamo1139 <adamo1139@gmail.com>
2025-09-18 01:07:53 +0200

    update

--- examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
+++ examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
@@ -71,7 +71,7 @@ MOE_ARGS=(
     --moe-grouped-gemm
     --moe-token-dispatcher-type alltoall
     --moe-router-dtype fp32
-    --num-experts 128
+    --num-experts 64
     --moe-ffn-hidden-size 1536
     --moe-router-score-function sigmoid
     --moe-router-topk 2
