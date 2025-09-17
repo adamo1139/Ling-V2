1b839dee8074871b7f1e2414e8adb7936c836803
adamo1139 <adamo1139@gmail.com>
2025-09-18 01:18:30 +0200

    update

--- examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
+++ examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
@@ -97,7 +97,7 @@ GPT_MODEL_ARGS=(
     --num-query-groups 2
     --group-query-attention
     --use-flash-attn
-    --max-position-embeddings 4096
+    --max-position-embeddings 6144
     --vocab-size 32000
     --make-vocab-size-divisible-by 128
     --position-embedding-type "rope"
@@ -117,7 +117,7 @@ GPT_MODEL_ARGS=(
 TRAINING_ARGS=(
     --micro-batch-size 8
     --global-batch-size 128
-    --seq-length 4096
+    --seq-length 6144
     --train-iters 167
     --weight-decay 0.1
     --adam-beta1 0.9
