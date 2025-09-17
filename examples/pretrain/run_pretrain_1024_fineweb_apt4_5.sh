5f5cc4e8ed553fc20d0942e6857e3fea20857ce0
adamo1139 <adamo1139@gmail.com>
2025-09-17 23:17:05 +0200

    update

--- examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
+++ examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
@@ -184,7 +184,7 @@ EVAL_AND_LOGGING_ARGS=(
 )
 
 KERNEL_ARGS=(
-    --attention-backend flash
+    --attention-backend auto
     --no-masked-softmax-fusion
     --attention-softmax-in-fp32	
     --cross-entropy-loss-fusion
