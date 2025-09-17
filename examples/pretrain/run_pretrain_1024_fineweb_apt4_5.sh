98aca2da620800597f99afdad85c68b06a1a3810
adamo1139 <adamo1139@gmail.com>
2025-09-17 22:26:23 +0200

    update

--- examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
+++ examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
@@ -148,7 +148,6 @@ MODEL_PARALLEL_ARGS=(
     --pipeline-model-parallel-size 1
     --tensor-model-parallel-size 1
     --sequence-parallel
-    --use-distributed-optimizer
     --recompute-granularity selective
     --recompute-modules moe
     
