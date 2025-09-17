41026eb3d350947fa8853219c483c58a2e4fa302
adamo1139 <adamo1139@gmail.com>
2025-09-17 23:05:01 +0200

    update

--- examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
+++ examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
@@ -82,7 +82,7 @@ MOE_ARGS=(
     --moe-router-group-topk 4
     --moe-z-loss-coeff 0.0000035
     --moe-router-bias-update-rate 1e-3
-    --moe-layer-freq [1,1,1,1,1,1,1,1]
+    --moe-layer-freq [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     --bias-zero-mean-update
 )
 
@@ -91,7 +91,7 @@ MPT_ARGS=(
 )
 
 GPT_MODEL_ARGS=(
-    --num-layers 8
+    --num-layers 16
     --hidden-size 4096
     --ffn-hidden-size 1024
     --num-attention-heads 16
