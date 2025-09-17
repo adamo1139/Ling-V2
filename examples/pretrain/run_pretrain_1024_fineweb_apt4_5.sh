c481d2c65b9837002f50cac5fb8bf886c77d867a
adamo1139 <adamo1139@gmail.com>
2025-09-18 01:27:28 +0200

    update

--- examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
+++ examples/pretrain/run_pretrain_1024_fineweb_apt4_5.sh
@@ -91,9 +91,9 @@ MPT_ARGS=(
 
 GPT_MODEL_ARGS=(
     --num-layers 4
-    --hidden-size 1280
+    --hidden-size 1024
     --ffn-hidden-size 1024
-    --num-attention-heads 10
+    --num-attention-heads 8
     --num-query-groups 2
     --group-query-attention
     --use-flash-attn
