65e50bcf0181149d23c8cc2b2b484d75c6fdf3fe
adamo1139 <adamo1139@gmail.com>
2025-09-22 15:40:22 +0200

    update

--- examples/pretrain/run_pretrain_poziomka_5_mini.sh
+++ examples/pretrain/run_pretrain_poziomka_5_mini.sh
@@ -2,7 +2,7 @@
 set -ex
 
 MODEL_PATH="" # no checkpoint needed for from-scratch training
-JOB_DIR="poziomka_5_mini_ep2"
+JOB_DIR="poziomka_5_mini_tp2"
 DATA_PATH="szypulka_tokenized_apt4_merged/apt4_merged_text_document"
 MEGATRON_PATH="Megatron-LM-core_v0.13.0"
 
@@ -66,7 +66,7 @@ if [ "$DEVICE_MODEL" = "NVIDIA GeForce RTX 3090 Ti" ] || [ "$DEVICE_MODEL" = "A1
 fi
 
 MOE_ARGS=(
-    --expert-model-parallel-size 2
+    --expert-model-parallel-size 1
     --expert-tensor-parallel-size 1
     --moe-grouped-gemm
     --moe-token-dispatcher-type alltoall
@@ -142,8 +142,10 @@ TRAINING_ARGS=(
 
 MODEL_PARALLEL_ARGS=(
     --pipeline-model-parallel-size 1
-    --tensor-model-parallel-size 1
+    --tensor-model-parallel-size 2
     --use-distributed-optimizer
+    --tp-comm-overlap
+    --sequence-parallel
     --overlap-param-gather
     --overlap-grad-reduce
 )
