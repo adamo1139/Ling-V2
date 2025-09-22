d06ae2a4a762d173e20d4f24bc61027acd78d142
adamo1139 <adamo1139@gmail.com>
2025-09-22 23:22:16 +0200

    update

--- examples/pretrain/run_pretrain_poziomka_5.sh
+++ examples/pretrain/run_pretrain_poziomka_5.sh
@@ -158,8 +158,8 @@ DATA_ARGS=(
 )
 
 EVAL_AND_LOGGING_ARGS=(
-    --save-interval 1500
-    --eval-interval 1500
+    --save-interval 300
+    --eval-interval 300
     --eval-iters 2
     --save $CHECKPOINT_PATH
     --ckpt-format "torch_dist"
