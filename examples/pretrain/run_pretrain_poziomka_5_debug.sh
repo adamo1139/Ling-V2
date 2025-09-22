368b9ad02d58813fa0bd687e4102d86bee7227db
adamo1139 <adamo1139@gmail.com>
2025-09-22 17:59:25 +0200

    update

--- examples/pretrain/run_pretrain_poziomka_5_debug.sh
+++ examples/pretrain/run_pretrain_poziomka_5_debug.sh
@@ -66,7 +66,7 @@ if [ "$DEVICE_MODEL" = "NVIDIA GeForce RTX 3090 Ti" ] || [ "$DEVICE_MODEL" = "A1
 fi
 
 MOE_ARGS=(
-    --expert-model-parallel-size 4
+    --expert-model-parallel-size 2
     --expert-tensor-parallel-size 1
     --moe-grouped-gemm
     --moe-token-dispatcher-type alltoall
@@ -142,7 +142,7 @@ TRAINING_ARGS=(
 
 MODEL_PARALLEL_ARGS=(
     --pipeline-model-parallel-size 1
-    --tensor-model-parallel-size 2
+    --tensor-model-parallel-size 4
     --sequence-parallel
     --overlap-grad-reduce
 )
