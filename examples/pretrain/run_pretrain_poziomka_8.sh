be086d896cfacd751886153791c2ffb99b329936
adamo1139 <adamo1139@gmail.com>
2026-01-30 06:09:52 +0100

    update

--- examples/pretrain/run_pretrain_poziomka_8.sh
+++ examples/pretrain/run_pretrain_poziomka_8.sh
@@ -91,7 +91,7 @@ MOE_ARGS=(
     --moe-ffn-hidden-size 320
     --moe-shared-expert-intermediate-size 320
     --moe-router-score-function sigmoid
-    --moe-router-topk 4
+    --moe-router-topk 8
     --moe-router-enable-expert-bias
     --moe-router-topk-scaling-factor 2.5
     --moe-router-num-groups 8
