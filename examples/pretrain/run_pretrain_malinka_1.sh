3101e831415e34f4156ed2033e259a59f33e8206
adamo1139 <adamo1139@gmail.com>
2026-01-31 16:44:03 +0100

    update

--- examples/pretrain/run_pretrain_malinka_1.sh
+++ examples/pretrain/run_pretrain_malinka_1.sh
@@ -68,7 +68,7 @@ if [ "$DEVICE_MODEL" = "NVIDIA GeForce RTX 3090 Ti" ] || [ "$DEVICE_MODEL" = "A1
 fi
 
 MOE_ARGS=(
-    --expert-model-parallel-size 2
+    --expert-model-parallel-size 1
     --expert-tensor-parallel-size 1
     --moe-grouped-gemm
     --moe-token-dispatcher-type alltoall
@@ -143,7 +143,7 @@ TRAINING_ARGS=(
 )
 
 MODEL_PARALLEL_ARGS=(
-    --pipeline-model-parallel-size 3
+    --pipeline-model-parallel-size 1
     --tensor-model-parallel-size 2
     --sequence-parallel
     --overlap-grad-reduce
