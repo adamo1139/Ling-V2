928dd8e5188409ebe999ecd5365eb934dcd38cf0
adamo1139 <adamo1139@gmail.com>
2026-02-01 23:59:45 +0100

    update

--- examples/pretrain/run_pretrain_malinka_1.sh
+++ examples/pretrain/run_pretrain_malinka_1.sh
@@ -79,7 +79,7 @@ MOE_ARGS=(
     --expert-model-parallel-size 1
     --expert-tensor-parallel-size 1
     --moe-grouped-gemm
-    --moe-token-dispatcher-type allgather
+    --moe-token-dispatcher-type alltoall
     --moe-router-dtype fp32
     --num-experts 8
     --moe-ffn-hidden-size 240
