15e1a1a398f7369dddf27225e7991727da02758c
adamo1139 <adamo1139@gmail.com>
2026-02-01 17:43:44 +0100

    update

--- examples/pretrain/run_pretrain_malinka_1.sh
+++ examples/pretrain/run_pretrain_malinka_1.sh
@@ -48,8 +48,8 @@ export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
 export NCCL_NVLS_ENABLE=0
 export NCCL_CUMEM_ENABLE=0
 
-export NVTE_FLASH_ATTN=0 # get that sweet FA2 boost
-export NVTE_FUSED_ATTN=1
+export NVTE_FLASH_ATTN=1 # get that sweet FA2 boost
+export NVTE_FUSED_ATTN=0
 export NVTE_UNFUSED_ATTN=0
 
 export NVTE_DEBUG=1
@@ -183,7 +183,7 @@ EVAL_AND_LOGGING_ARGS=(
 )
 
 KERNEL_ARGS=(
-    --attention-backend fused
+    --attention-backend flash
     --no-masked-softmax-fusion
     --attention-softmax-in-fp32	
     --cross-entropy-loss-fusion
