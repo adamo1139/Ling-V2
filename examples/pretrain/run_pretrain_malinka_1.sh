0b9bcc12ef60bd409fc0bc59485fbef482100209
adamo1139 <adamo1139@gmail.com>
2026-02-02 02:04:57 +0100

    update

--- examples/pretrain/run_pretrain_malinka_1.sh
+++ examples/pretrain/run_pretrain_malinka_1.sh
@@ -82,8 +82,8 @@ MOE_ARGS=(
     --moe-token-dispatcher-type alltoall
     --moe-router-dtype fp32
     --num-experts 24
-    --moe-ffn-hidden-size 384
-    --moe-shared-expert-intermediate-size 384
+    --moe-ffn-hidden-size 512
+    --moe-shared-expert-intermediate-size 512
     --moe-router-score-function sigmoid
     --moe-router-topk 6
     --moe-router-enable-expert-bias
