3df9a0000f96583b6fe9407e9ac99412ce77d09a
adamo1139 <adamo1139@gmail.com>
2026-03-02 13:12:37 +0100

    update

--- examples/pretrain/run_pretrain_malinka_1.sh
+++ examples/pretrain/run_pretrain_malinka_1.sh
@@ -155,7 +155,7 @@ TRAINING_ARGS=(
 )
 
 MODEL_PARALLEL_ARGS=(
-    --pipeline-model-parallel-size 2
+    --pipeline-model-parallel-size 4
     --tensor-model-parallel-size 1
     --sequence-parallel
     --use-distributed-optimizer
