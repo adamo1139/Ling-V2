62ddc6c6dc30cddd02f755247a8a1c786b636c59
adamo1139 <adamo1139@gmail.com>
2026-03-02 17:21:25 +0100

    update

--- examples/pretrain/run_pretrain_malinka_1.sh
+++ examples/pretrain/run_pretrain_malinka_1.sh
@@ -92,7 +92,7 @@ MOE_ARGS=(
     --moe-router-group-topk 1
     --moe-z-loss-coeff 0.0000035
     --moe-router-bias-update-rate 1e-3
-    --moe-layer-freq [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    --moe-layer-freq [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     --bias-zero-mean-update
     --moe-expert-capacity-factor 1.25
     --moe-pad-expert-input-to-capacity
@@ -108,7 +108,7 @@ MPT_ARGS=(
 )
 
 GPT_MODEL_ARGS=(
-    --num-layers 32
+    --num-layers 64
     --hidden-size 4096
     --ffn-hidden-size 4096
     --num-attention-heads 32
