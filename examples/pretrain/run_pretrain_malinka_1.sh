d08ccd32384f3482f4001ef0b161ce3f1db06799
adamo1139 <adamo1139@gmail.com>
2026-03-02 13:41:07 +0100

    update

--- examples/pretrain/run_pretrain_malinka_1.sh
+++ examples/pretrain/run_pretrain_malinka_1.sh
@@ -111,8 +111,8 @@ GPT_MODEL_ARGS=(
     --num-layers 32
     --hidden-size 2048
     --ffn-hidden-size 2048
-    --num-attention-heads 16
-    --num-query-groups 16
+    --num-attention-heads 8
+    --num-query-groups 8
     --group-query-attention
     --qk-layernorm
     --use-flash-attn
