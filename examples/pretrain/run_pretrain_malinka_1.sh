02938b1198a6b2911156d32df120f21f7106909f
adamo1139 <adamo1139@gmail.com>
2026-03-02 13:31:11 +0100

    update

--- examples/pretrain/run_pretrain_malinka_1.sh
+++ examples/pretrain/run_pretrain_malinka_1.sh
@@ -92,7 +92,7 @@ MOE_ARGS=(
     --moe-router-group-topk 1
     --moe-z-loss-coeff 0.0000035
     --moe-router-bias-update-rate 1e-3
-    --moe-layer-freq [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    --moe-layer-freq [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     --bias-zero-mean-update
     --moe-expert-capacity-factor 1.25
     --moe-pad-expert-input-to-capacity
